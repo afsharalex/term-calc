9040397ea81a714c56fe28838cfc4f6b286e7c55
Alex Afshar <contact@alexafshar.com>
2022-05-19 03:20:54 -0400

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::io::{self, stdout, Write};
+use std::fmt;
+
+struct Token {
+    token_type: String,
+    literal: String,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "<{}: {}>", self.token_type, self.literal)
+    }
+}
+
+fn main() {
+    println!("Welcome to TC - the Terminal Calculator!");
+
+    loop {
+        print!("TC> ");
+
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{} bytes read", n);
+                println!("{}", input);
+
+                if input.trim() == "quit" || input.trim() == "q" {
+                    println!("quitting...");
+                    break;
+                }
+
+                let tokens = tokenize(&input);
+
+                println!("Tokens:");
+                for token in tokens {
+                    println!("{}", token);
+                }
+            }
+            Err(error) => println!("error: {}", error),
+        }
+
+    }
+}
+
+fn make_number(num_str: String) -> Token {
+    return Token{
+        token_type: "number".to_string(),
+        literal: num_str,
+    };
+}
+
+fn tokenize(source: &str) -> Vec<Token> {
+    let source_chars: Vec<_> = source.chars().collect();
+
+    let mut tokens: Vec<Token> = Vec::new();
+
+    let mut i = 0;
+
+    while i < source_chars.len() - 1 {
+        let c = source_chars[i];
+        i += 1;
+        match c {
+            ' ' => continue,
+            'A'..='Z' | 'a'..='z' => {
+                println!("Got Char: {}", c);
+                continue;
+            },
+            '0'..='9' => {
+                println!("Got Digit: {}", c);
+
+                let mut num_str: String = String::from(c);
+
+                if source_chars[i].is_digit(10) && i < source_chars.len() - 1 {
+                    println!("Incoming digit: {}", source_chars[i]);
+
+                    while source_chars[i].is_digit(10) && i < source_chars.len() - 1 {
+                        num_str.push_str(&String::from(source_chars[i]));
+                        i += 1;
+                    }
+
+                    println!("Got: {}", num_str);
+                    tokens.push(make_number(num_str));
+                }
+            },
+            _ => continue,
+        }
+    }
+
+
+    return tokens;
+}
